b84600ee34b33e1c22f1ab9f0baed763a887e9e0
Islom <ulmasov_i@yahoo.com>
2022-03-31 21:00:50 +0300

    Add new function

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// everythink is correct
+let me1 = {
+    name: 'jnjn'
+}
+
+let me2 = {
+    name: 'jnjn'
+}
+
+let me3 = me1
+me3.name = '11111'
+console.log(me1 === me3)
+console.log(me1.name)
+console.log(me3.name)
+
+
+console.log(me1 === me2 )
+console.log(me1.name === me2.name )
+
+let name = {
+    name: 'Shokhrukh',
+    surName: 'Akramov'
+}
+
+console.log(name);
